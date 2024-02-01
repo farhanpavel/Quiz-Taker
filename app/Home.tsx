56e2afd3d9a80ee1a199ea5539bbdd1e9b468470
farhanpavel <farhanpavel3@gmail.com>
2024-02-02 00:43:21 +0600

    pic added

--- app/Home.tsx
+++ app/Home.tsx
@@ -115,7 +115,7 @@ const Home = () => {
                       <div>
                         <img
                           className="h-96 max-w-full rounded-lg object-cover object-center "
-                          src="https://images.unsplash.com/photo-1629367494173-c78a56567877?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=927&q=80"
+                          src="./images/Home/pavel.jpg"
                           alt="gallery-photo"
                         />
                       </div>
