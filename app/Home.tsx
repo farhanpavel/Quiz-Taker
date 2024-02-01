fd709faf01d42fe4dfb5e0d1bb0be56853f733dd
farhanpavel <farhanpavel3@gmail.com>
2024-02-02 00:37:38 +0600

    Responsive Feature added

--- app/Home.tsx
+++ app/Home.tsx
@@ -5,24 +5,27 @@ const Home = () => {
     <div>
      
      <div className='md:flex md:flex-wrap md:flex-row justify-between p-5 shadow-lg font-roboto font-bold '>
-      <div className='flex flex-wrap gap-x-3'>
+      <div className='flex flex-wrap items-center gap-x-3'>
       
        <img className='w-9' src="./images/Home/Q.png" alt="" />
-        <h1 className='text-2xl font-extralight'>Quizzy</h1>
+        <h1 className='text-2xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-indigo-400 to-cyan-400'>Quizzy</h1>
        </div>
       <div>
-      <div className='md:hidden float-right -mt-8 border-2 border-black opacity-90 shadow-lg rounded'>
-       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-5 h-5 ">
-      <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5" />
-      </svg>
+      <div className='md:hidden float-right -mt-8  '>
+      <button data-collapse-toggle="mobile-menu" type="button" className="md:hidden ml-3 text-black hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-blue-300 rounded-lg inline-flex items-center justify-center" aria-controls="mobile-menu-2" aria-expanded="false">
+        <span className="sr-only">Open main menu</span>
+        <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
+        <svg className="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+        </button>
        </div>
       </div>
        
-      <div className='mt-4 md:mt-0'>
+      <div className='mt-4 md:mt-0' id="mobile-menu">
       <ul className='md:flex md:flex-wrap md:space-y-0 space-y-3 gap-x-10 text-gray-600 text-sm '>
-        <li className='hover:bg-[#0467FF] p-2 hover:text-white hover:rounded'><a href="">How It Works?</a></li>
-        <li  className='hover:bg-[#0467FF] p-2 hover:text-white  hover:rounded'><a href="">Features</a></li>
-        <li  className='hover:bg-[#0467FF] p-2 hover:text-white  hover:rounded'><a href="">About us</a></li>
+        
+        <li className='p-2' ><a className='p-2 hover:rounded hover:bg-[#0467FF] hover:text-white '  href="">How It Works?</a></li>
+        <li className='p-2'><a className='p-2 hover:rounded hover:bg-[#0467FF] hover:text-white ' href="">Features</a></li>
+        <li className='p-2'><a className='p-2 hover:rounded hover:bg-[#0467FF] hover:text-white '  href="">About us</a></li>
         <button  className='hover:bg-[#06FE8E] hover:text-white border-2 rounded-full border-[#06FE8E] px-5 py-1 text-[#06FE8E]  -mt-1'><a href="">Login</a></button>
       </ul>
       </div>
@@ -30,7 +33,7 @@ const Home = () => {
     <div className='container mx-auto'>
       <div className='flex flex-wrap justify-around'>
         <div className='m-auto '>
-        <h1 className='lg:text-4xl mt-3 text-lg sm:text-2xl md:text-3xl w-3/4 bg-clip-text  text-transparent bg-gradient-to-r from-[#0178ff] to-[#49CDFF]'>Learn new concepts each minute</h1>
+        <h1 className='font-semibold lg:text-4xl mt-3 text-lg sm:text-2xl md:text-3xl w-3/4 bg-clip-text  text-transparent bg-gradient-to-r from-[#0178ff] to-[#49CDFF]'>Learn new concepts each minute</h1>
         <div className='border-l-4 border-[#01FF88] '>
         <p className='mt-5 text-md  md:text-md  mx-2 text-[#828282]'>We help you prepare for exams and quizes </p>
         
@@ -138,12 +141,12 @@ const Home = () => {
                     
                   </div>
 
-                {/* </div> */}
+              
             
                 <div className=''>
                     <div>
                        
-                        <p className='md:text-6xl font-bold '>A House Of Creative & Intelligent</p> <br/>
+                        <p className='md:text-6xl font-bold bg-clip-text  text-transparent bg-gradient-to-r from-[#01FF88] to-[#56CCF2] py-3 -ms-1'>A House Of Creative & Intelligent</p> <br/>
                         <p className='md:text-xl'>In social Science is defined as the capacity of individuals to act independtly and to influence  that derermine or limit.</p>
                     </div>
                     <br/>
@@ -155,28 +158,28 @@ const Home = () => {
 </div>
     <br />
     <div className='mt-32 '>
-    <p className='text-4xl text-center mb-16'>Let's checkout your <br />
-<span className='text-blue-600'>learning</span> journey</p>
+    <p className='text-4xl text-center mb-16 font-bold'>Let's checkout your <br />
+<span className='bg-clip-text  text-transparent bg-gradient-to-r from-[#0178ff] to-[#49CDFF]'>learning</span> journey</p>
     <div className='flex flex-wrap justify-around'>
       <div className='md:w-1/4 sm:1/2'>
-      <div >
+      <div  >
       <img src="./images/Home/Group 13.png" alt="" />
       <h1>Hello word</h1>
       <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia eveniet eius veritatis doloremque veniam sunt nesciunt sit sed excepturi nemo.</p>
       </div>
+      <div  >
+      <img src="./images/Home/Group 14.png" alt="" />
+      <h1>Hello word</h1>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia eveniet eius veritatis doloremque veniam sunt nesciunt sit sed excepturi nemo.</p>
+      </div>
+      </div>
+      <div className=' md:w-1/4 sm:1/2 '>
       <div>
       <img src="./images/Home/Group 15.png" alt="" />
       <h1>Hello word</h1>
       <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia eveniet eius veritatis doloremque veniam sunt nesciunt sit sed excepturi nemo.</p>
       </div>
-      </div>
-      <div className=' md:w-1/4 sm:1/2'>
-      <div>
-      <img src="./images/Home/Group 14.png" alt="" />
-      <h1>Hello word</h1>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia eveniet eius veritatis doloremque veniam sunt nesciunt sit sed excepturi nemo.</p>
-      </div>
-      <div>
+      <div >
       <img src="./images/Home/Group 16.png" alt="" />
       <h1>Hello word</h1>
       <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia eveniet eius veritatis doloremque veniam sunt nesciunt sit sed excepturi nemo.</p>
@@ -184,39 +187,32 @@ const Home = () => {
       
       </div>
      
-      <div>
-    <img src="./" alt="" />
-      </div>
-      <div className='w-1/4 ms-10 mt-8 invisible sm:visible'>
+      <div className='w-1/4 ms-32 mt-8 md:block hidden'>
       <div><img className='w-24 relative right-20' src="./images/Home/Group 26.png" alt="" /></div>
-      <div><img className='w-24  relative left-20' src="./images/Home/Group 26.png" alt="" /></div>
-      <div><img className='w-24  relative right-20' src="./images/Home/Group 26.png" alt="" /></div>
-      <div><img className='w-24  relative left-20' src="./images/Home/Group 26.png" alt="" /></div>
+      <div><img className='w-24  relative left-20' src="./images/Home/Group 27.png" alt="" /></div>
+      <div><img className='w-24  relative right-20' src="./images/Home/Group 28.png" alt="" /></div>
+      <div><img className='w-24  relative left-20' src="./images/Home/Group 29.png" alt="" /></div>
       </div>
     </div>
     
     
     </div>
-
-    <div className='md:flex md:flex-wrap md:flex-row justify-between p-5 shadow-lg font-roboto font-bold '>
-      <div className='flex flex-wrap gap-x-3'>
-      
-       <img className='w-9' src="./images/Home/Q.png" alt="" />
-        <h1 className='text-2xl font-extralight'>Quizzy</h1>
-       </div>
-      <div>
-      </div>   
-      <div className='mt-4 md:mt-0'>
-      <ul className='md:flex md:flex-wrap md:space-y-0 space-y-3 gap-x-10 text-gray-600 text-sm '>
-        <li className=' p-1/2'><img className='w-9 relative right-20' src="/public/images/Home/Group 22.png" alt="" /></li>
-        <li  className='p-1/2'><img className='w-9 relative right-20' src="/public/images/Home/Layer 2.png" alt="" /></li>
-        <li  className='p-1/2'><img className='w-9 relative right-20' src="/public/images/Home/Layer 2 (1).png" alt="" /></li>
-        <li  className='p-1/2'><img className='w-9 relative right-20' src="/public/images/Home/Vector.png" alt="" /></li>
-
-      </ul>
+    <div className='sm:flex sm:flex-wrap p-5 sm:justify-between  bg-[#fafafa]'>
+      <div className='flex flex-wrap gap-1 items-center place-content-center'>
+      <img className='w-9' src="./images/Home/Q.png" alt="" />
+      <h1 className='text-2xl font-bold font-roboto bg-clip-text text-transparent bg-gradient-to-r from-indigo-400 to-cyan-400'>Quizzy</h1>
       </div>
-     </div>
-     <h1>Pavel</h1>
+      <div >
+        <ul  className='flex flex-wrap mt-8 sm:mt-0 place-content-center gap-x-5'> 
+          <li><a href=""><img className='w-8' src="./images/Home/insta.png" alt="" /></a></li>
+          <li><a href=""><img className='w-8' src="./images/Home/linkedin.png" alt="" /></a></li>
+          <li><a href=""><img className='w-8' src="./images/Home/ball.png" alt="" /></a></li>
+          <li><a href=""><img className='w-8' src="./images/Home/twitter.png" alt="" /></a></li>
+        </ul>
+      </div>
+     
+    </div>
+   
 </div>
 
 
